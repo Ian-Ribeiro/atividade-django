645c3a90c0c2caa1a643d03eb932e9d03e4ff825
Ian-Ribeiro <ianmaxrodrigues27@gmail.com>
2025-06-27 16:40:05 -0300

    More a commit

--- app/views.py
+++ app/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    #return HttpResponse("<h1>Seja Bem-Vind!!! o Ao Django</h1>")#
     return render(request,'index.html')
+
+def index2(request):
+    return render(request,'index2.html')
--- setup/urls.py
+++ setup/urls.py
@@ -19,7 +19,9 @@ from django.urls import path
 
 from app import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('index.html', views.index),
+    path('index2.html', views.index2)
 ]
